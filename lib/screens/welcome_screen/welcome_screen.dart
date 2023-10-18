cd6e48f7c506e643bd50e3278f2a5f884f20267f
Audrey Chanakira <tate.audrie.charks@gmail.com>
2023-10-18 14:43:02 +0200

    Implemented basic setup

--- lib/screens/welcome_screen/welcome_screen.dart
+++ lib/screens/welcome_screen/welcome_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../../stateless_widgets/custom_button.dart';
+import '../../theme/theme_colors.dart';
+import '../../theme/theme_text_styles.dart';
+import '../../utils/helper_size_widgets.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -12,6 +18,73 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background-image.jpg"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween, // Pushes content to top and bottom
+          children: [
+            Column(
+              children: [
+                addVerticalSpace(100),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Text(
+                    "Attendance",
+                    softWrap: true,
+                    style: kLargeLabelText,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Text(
+                    "App",
+                    softWrap: true,
+                    style: kMediumLabelText,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                addVerticalSpace(20),
+              ],
+            ),
+
+            Lottie.asset(
+              'assets/animation/animation.json', // Replace with the path to your Lottie animation file
+              width: 400, // Set the width
+              height: 400, // Set the height
+            ),
+
+
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 150),
+                child: CustomButton.defaultButton(
+                  text: 'GET STARTED',
+                  onPressed: () {
+                    // Handle button click
+                  },
+                  backgroundColor: kAccent,
+                  textColor: Colors.white,
+                  borderColor: Colors.white,
+                  cornerRadius: 20,
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/theme/theme_text_styles.dart
@@ -0,0 +1,79 @@
+import 'package:dev_junction_attendance_app/theme/theme_colors.dart';
+import 'package:flutter/material.dart';
+
+
+//App Bar
+const kLargeAppTitle = TextStyle(
+    color: kBlackTextColor,
+    fontSize: 24,
+    fontFamily: 'Neo_Sans_W1G');
+
+//TEXT_FIELDS
+const kSmallLabelText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 10,
+    color: kDarkTextColor);
+
+const kSemiMediumLabelText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 14,
+    color: kDarkTextColor);
+
+const kStandardLabelText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 16,
+    color: kBlackTextColor);
+
+const kProfileLabelText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 35,
+    color: kWhiteTextColor);
+
+const kMediumLabelText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 20,
+    color: kAccent);
+
+const kLargeLabelText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 30,
+    color: kAccent);
+
+const kHeaderText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 30,
+    fontWeight: FontWeight.w100,
+    color: kDarkTextColor);
+
+
+const kButtonStandardLabelTextWhite = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 16,
+    color: kAccent);
+
+const kButtonStandardLabelTextBlack = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 16,
+    color: kAccent);
+
+//Cell Text
+const kHeaderCellText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: kDarkTextColor);
+
+const kSubHeaderCellText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 10,
+    fontWeight: FontWeight.w100,
+    color: kDarkTextColor);
+
+const kAddressCellText = TextStyle(
+    fontFamily: 'Neo_Sans_W1G',
+    fontSize: 12,
+    fontWeight: FontWeight.w100,
+    color: kDarkTextColor);
+
+
+
